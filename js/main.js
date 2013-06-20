2c1055e2f77aa5e97709e24705643bebdff0f3c3
adriaanAKQA <adriaan.scholvinck@akqa.com>
2013-06-20 12:21:25 -0400

    a new format syntax for controllers that looks cleaner and easier to read

--- js/main.js
+++ js/main.js
@@ -1,54 +1,55 @@
 
-function CarouselCtrl($scope) {
-	$scope.carousel = [
-		{header:'Item One', paragraph:'Lorem ipsum dolar', image:'', href:''},
-		{header:'Item Two', paragraph:'Lorem ipsum dolar', image:'', href:''},
-		{header:'Item Three', paragraph:'Lorem ipsum dolar', image:'', href:''},
-		{header:'Item Four', paragraph:'Lorem ipsum dolar', image:'', href:''},
-		{header:'Item Five', paragraph:'Lorem ipsum dolar', image:'', href:''},
-		{header:'View All', paragraph:'Lorem ipsum dolar', image:'', href:''}
-	];
+var app = angular.module( "demo", [] );
 
-	$scope.goNext = function() {
-		// go to next page
-		console.log ( "NEXT!" );
-	};
+app.controller(
+	'CarouselCtrl',
+	function( $scope ) {
+		$scope.carousel = [
+			{header:'Item One', paragraph:'Lorem ipsum dolar', image:'', href:''},
+			{header:'Item Two', paragraph:'Lorem ipsum dolar', image:'', href:''},
+			{header:'Item Three', paragraph:'Lorem ipsum dolar', image:'', href:''},
+			{header:'Item Four', paragraph:'Lorem ipsum dolar', image:'', href:''},
+			{header:'Item Five', paragraph:'Lorem ipsum dolar', image:'', href:''},
+			{header:'View All', paragraph:'Lorem ipsum dolar', image:'', href:''}
+		];
 
-	$scope.goPrev = function() {
-		// go to prev page
-		console.log ( "PREV!" );
-	};
+		$scope.goNext = function() {
+			// go to next page
+			console.log ( "NEXT!" );
+		};
 
-	$scope.carouselWidth = 'width:' + window.innerWidth * $scope.carousel.length + 'px';
-	$scope.carouselItemWidth = 'width:' + window.innerWidth + 'px;';
-}
+		$scope.goPrev = function() {
+			// go to prev page
+			console.log ( "PREV!" );
+		};
 
-function MainNavCtl($scope) {
-	$scope.carousel = [
-		{header:'Home', paragraph:'Lorem ipsum dolar', image:''},
-		{header:'Section 1', paragraph:'Lorem ipsum dolar', image:''},
-		{header:'Section 2', paragraph:'Lorem ipsum dolar', image:''},
-		{header:'Section 3', paragraph:'Lorem ipsum dolar', image:''},
-		{header:'Section 4', paragraph:'Lorem ipsum dolar', image:''},
-		{header:'Settings', paragraph:'Lorem ipsum dolar', image:''}
-	];
-}
-function tellAngular() {
-	var domElt = document.getElementById('carousel');
-	$scope = angular.element(domElt).scope();
-	$scope.$apply(function() {
-		$scope.carouselWidth = 'width:' + window.innerWidth * $scope.carousel.length + 'px';
-		$scope.carouselItemWidth = 'width:' + window.innerWidth + 'px;';
+		$scope.resize = function() {
+			console.log ( 'carousel resize!' );
+			$scope.carouselWidth = 'width:' + window.innerWidth * $scope.carousel.length + 'px';
+			$scope.carouselItemWidth = 'width:' + window.innerWidth + 'px;';
+		};
+
+		$scope.resize();
 	});
-}
-window.onresize = tellAngular;
 
-angular.module('demo', []).
-	config(function($routeProvider) {
+app.controller(
+	'MainNavCtrl',
+	function($scope) {
+		$scope.nav = [
+			{header:'Home', paragraph:'Lorem ipsum dolar', image:''},
+			{header:'Section 1', paragraph:'Lorem ipsum dolar', image:''},
+			{header:'Section 2', paragraph:'Lorem ipsum dolar', image:''},
+			{header:'Section 3', paragraph:'Lorem ipsum dolar', image:''},
+			{header:'Section 4', paragraph:'Lorem ipsum dolar', image:''},
+			{header:'Settings', paragraph:'Lorem ipsum dolar', image:''}
+		];
+	});
+
+app.config(function($routeProvider) {
 		console.log ( "GO");
 		$routeProvider.
 			// when('/', {controller:ListCtrl, templateUrl:'list.html'}).
 			// when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
-			when('/guide', {controller:CarouselCtrl, templateUrl:'guide.html'}).
+			when('/guide', {controller:app.CarouselCtrl, templateUrl:'guide.html'}).
 			otherwise({redirectTo:'/'});
-	})
+	});
