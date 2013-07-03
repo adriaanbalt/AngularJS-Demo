42f0afc866717eb4ee00b4a8babe15b6a4c23cd4
adriaanbalt <adriaan@balt.us>
2013-07-03 11:42:00 -0400

    the carousel is working again and there is less syntax

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
  * @name app.demo
  * @description construct the wrapper for the DEMO app
  */
-var app = angular.module( "demo", [] );
+angular.module( "demo", [] )
 
 /**
  * @name app.controller.CarouselCtrl
  * @description creates a carousel 
  */
-app.controller(
+.controller(
 	'CarouselCtrl',
 	function( $scope, $routeParams, $location, $http, $timeout, index ) {
 		$http.get('../carousel.json')
@@ -27,19 +27,23 @@ app.controller(
 		 */
 		$scope.gotoItem = function( index ) {
 			if ( index < 0 ) index = 0;
-			if ( index >= $scope.carousel.length ) index = $scope.carousel.length-1;
-			if ( !index ) index = currentIndex;
+			if ( index > $scope.carousel.length-1 ) index = $scope.carousel.length-1;
+			//if ( !index ) index = currentIndex;
+			
 			// find the difference between where the carousel is and where it should go
 			deltaIndex = Math.abs(currentIndex - index);
 
 			// store new index
-			// currentIndex = index;
+			currentIndex = index;
+			
 			// animate to the designated index over a duration based on the difference between them
 			$scope.transitionDuration = deltaIndex * .5;
+			
 			// increment
 			if ( -($scope.carouselWidth-$scope.carouselItemWidth) <= $scope.newPos && $scope.newPos <= 0 || $scope.newPos === undefined ) {
-				$scope.newPos = -(windowWidth*(currentIndex-1));
+				$scope.newPos = -(windowWidth*(currentIndex));
 			} 
+			
 			// update URL hash
 			$location.hash( currentIndex );
 		};
@@ -52,7 +56,7 @@ app.controller(
 		$scope.clickToItem = function( index ){
 			// $timeout.cancel(timeout);
 			// timeout = $timeout( autoplay, 2000 );
-			$scope.gotoItem(index);
+			$scope.gotoItem(index-1);
 		};
 
 		/**
@@ -63,7 +67,7 @@ app.controller(
 			// $timeout.cancel( timeout );
 			// timeout = $timeout( autoplay, 2000 );
 			$scope.transitionDuration = .5;
-			$scope.gotoItem( );
+			$scope.gotoItem( currentIndex+1 );
 			// go to next page
 			//if ( $scope.newPos > -($scope.carouselWidth-$scope.carouselItemWidth) ) $scope.newPos -= windowWidth;
 		};
@@ -73,9 +77,9 @@ app.controller(
 		 * @description a function called from the DOM PREVIOUS button
 		 */
 		$scope.goPrev = function() {
-			$timeout.cancel( timeout );
+			// $timeout.cancel( timeout );
 			$scope.transitionDuration = .5;
-			$scope.gotoItem( currentIndex-- );
+			$scope.gotoItem( currentIndex-1 );
 			// go to prev page
 			//if ( $scope.newPos < 0 ) $scope.newPos += windowWidth;
 		};
@@ -99,7 +103,8 @@ app.controller(
 			$scope.carouselWidth = windowWidth * $scope.carousel.length;
 			$scope.carouselItemWidth = windowWidth;
 			$scope.transitionDuration = .5;
-			currentIndex = $location.hash();
+			currentIndex = $location.hash() === '' ? 0 : $location.hash();
+
 			// first load up automatically repositions the carousel based on hash
 			$scope.gotoItem( $location.hash() );
 		};
@@ -115,33 +120,33 @@ app.controller(
 
 		// timeout = $timeout( autoplay, 1500 );
 
-	});
+	})
 
 /**
  * @name app.factory.index
  * @description if you research Factory in Angular you can create a key variable that wraps the DOM to the scope of the app.  but it's a new idea so im not sure.
  * references: http://stackoverflow.com/questions/14324451/angular-service-vs-angular-factory
  */
-app.factory(
+.factory(
 	'index',
 	function() {
 
-	});
+	})
 /**
  * @name app.controller.TextArea
  * @description the plan with this is that the TextArea at the bottom of the screen with the scrolling area is bound to the page via the INDEX, which will update on index change.
  */
-app.controller(
+.controller(
 	'TextArea',
 	function( $scope, index ){
 
-	});
+	})
 
 /**
  * @name app.controller.MainNavCtrl
  * @description creates the navigation in the DOM
  */
-app.controller(
+.controller(
 	'MainNavCtrl',
 	function( $scope, $http ) {
 		// gets the data for the nav
@@ -153,40 +158,40 @@ app.controller(
 		$scope.resize = function() {
 			console.log ( 'MainNavCtrl resize!' );
 		};
-	});
+	})
 
 /**
  * @name app.directive.resize
  * @description trying to modularize a single source of resize events rather than linking up a bunch to the DOM.  this sadly isn't working as well as i'd like.
  */
-app.directive(
-	'resize',
-	function() {
-		restrict: 'A', // restrict to ATTRIBUTE (could be E for "element")
-		function link( $scope, element, attributes ) {
-			window.onresize = function() {
-				$scope.$apply($scope.resize);
-			}
-		};
-		return({
-			link: link
-		});
-	});
+// .directive(
+// 	'resize',
+// 	function() {
+// 		restrict: 'A', // restrict to ATTRIBUTE (could be E for "element")
+// 		function link( $scope, element, attributes ) {
+// 			window.onresize = function() {
+// 				$scope.$apply($scope.resize);
+// 			}
+// 		};
+// 		return({
+// 			link: link
+// 		});
+// 	})
 
 /**
  * @name app.config
  * @description sets up the routes to sub templates, urls, and hashs
  */ 
-app.config(function( $routeProvider, $locationProvider ) {
+.config(function( $routeProvider, $locationProvider ) {
 	// $locationProvider.html5Mode(true);
 	$routeProvider.
 		when('/guide/:index', {
-			controller:app.CarouselCtrl, 
+			controller:'CarouselCtrl', 
 			templateUrl:'guide.html',
 			reloadOnSearch: false }
 		).
 		when('/guide', {
-			controller:app.CarouselCtrl, 
+			controller:'CarouselCtrl', 
 			templateUrl:'guide.html',
 			reloadOnSearch: false }
 		).
